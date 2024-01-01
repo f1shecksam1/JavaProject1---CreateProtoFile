d26cf05215d46a1391398a3c6000ab17e3a1c346
c-candycane <huseyinozcan5507@hotmail.com>
2024-01-01 22:47:46 +0300

    Commit1

--- JavaP/Program.java
+++ JavaP/Program.java
@@ -6,69 +6,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-
 //değişken isimlerinde türkçe harf kontrolü, değişken isimleri (int, float, double olamaz,) değişken isimleri özel karakter ile başlayamaz,
 // değişken ismi kontrolünü dosya adı için de çağır
 
 public class Program {
-    public static void setdefaultMessage(String userInput, List<String> lines , List<String>  messageVeriables,List<String> messageVeriablesName){
-        String messageBase = "message " + userInput + "{";
-        lines.add(messageBase);
-        for (int i = 0;i<messageVeriables.size();i++){
-            String messageVeriables1 =  messageVeriables.get(i) + " " + messageVeriablesName.get(i) + " = " + (i+1);
-        }
-
-    }
-
-    public static String getVarType(ScalarValuesBase base, String input){
-        try {
-            for (String key : base.scalarValuesMap.keySet()) {
-                String value = base.scalarValuesMap.get(key);
-                if (key.equals(input)) {
-                    System.out.println(value);
-                    return value;
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return "";
-    }
-    public static boolean messageControl(String inputMessage){
-        if (inputMessage.isEmpty() ||  Character.isDigit(inputMessage.charAt(0)) ||  inputMessage.contains(" ")
-        || inputMessage.equals("int")
-        || inputMessage.equals("double")
-        || inputMessage.equals("float")
-        || inputMessage.equals("char")
-        || inputMessage.equals("boolean")
-        || inputMessage.equals("String"))
-        {
-            System.out.println("Hata: Girişte başında sayı ve aralarda boşluk bulunmamalıdır.");
-            return false;
-        };
-        return true;
-    }
-
-    public static String makeFirstCharBig(String str) {
-        if (str == null || str.isEmpty()) {
-            return str;
-        }
-
-        // Tüm harfleri küçük yap
-        String kucukHarfString = str.toLowerCase();
-
-        // İlk harfi büyük yap
-
-        return Character.toUpperCase(kucukHarfString.charAt(0)) + kucukHarfString.substring(1);
-    }
-                            
-                        
 
     public static void main(String[] args) {
         ScalarValuesCSharp csharp;
         ScalarValuesCPP cpp;
         ScalarValuesJAVA java;
         ScalarValuesPython python;
+        ProgramFunctions programFunctions = new ProgramFunctions();
+
 
         csharp = new ScalarValuesCSharp();
         cpp = new ScalarValuesCPP();
@@ -76,20 +25,9 @@ public class Program {
         python = new ScalarValuesPython();
 
         try (Scanner input = new Scanner(System.in)) {
-            System.out.print("Please write your programming language (C#, C++, Java, Python): ");
-            String programmingLanguage = input.nextLine();
-            programmingLanguage = makeFirstCharBig(programmingLanguage);
-            while (!programmingLanguage.equals("C#") && !programmingLanguage.equals("C++") && !programmingLanguage.equals("Java") && !programmingLanguage.equals("Python")) {
-                System.out.println("Gecersiz dil girdiniz. Lutfen tekrar giriniz.");
-                System.out.print("Please write your programming language (C#, C++, Java, Python): ");
-                programmingLanguage = input.nextLine();
-            }
-            System.out.print("Please write your proto file name: ");
-            String protoFileName = input.nextLine();
+            String programmingLanguage = programFunctions.ChooseProgramingLanguage(input);
+            String protoFileName = programFunctions.SetProtoFileName(input);
             String protoFileNameWithProto = protoFileName + ".proto";
-            String selectedLanguage="";
-
-            // programmingLanguage control yap
 
             try {
                 Path path = Paths.get(protoFileNameWithProto);
@@ -102,145 +40,110 @@ public class Program {
                     System.out.println("New proto file created.");
                     Files.createFile(path);
 
-                    lines.add("syntax = \"proto3\";");
-                    lines.add("");
-                    lines.add("package " + protoFileName+";");
-                    lines.add("\n");
-                    //Files.write(path, lines);
-                    List<String> messageList = new ArrayList<>();
+                    programFunctions.AddBaseStringToProto(lines, protoFileName);
+                    List<String> messageNames = new ArrayList<>();
                     try {
-                        while(true){
+                        while (true) {
                             System.out.print("Mesajınızı giriniz. Cikmak icin bitti yaziniz: ");
-                            String girilenMessage = input.nextLine();
-                            if(girilenMessage.equals("bitti")){
+                            String inputMessage = input.nextLine();
+                            if (inputMessage.equals("bitti")) {
                                 System.out.println("Cıkıs yapıldı");
                                 break;
                             }
-                            if(!messageControl(girilenMessage)){
+                            if (!ProgramFunctions.ControlFunctions.messageControl(inputMessage) ||
+                                    (programFunctions.ControlListContain(messageNames,
+                                            "Daha önce kullanılmış bir mesaj adı girdiniz. Lütfen tekrar deneyin.",
+                                            inputMessage)) ) {
                                 continue;
                             }
-                            lines.add("message "+girilenMessage+"{ ");
-                            messageList.add(girilenMessage);
-                            int varCount=1;
+                            lines.add(programFunctions.OpenRpcMessage(inputMessage));
+                            messageNames.add(inputMessage);
+                            int varCount = 1;
                             List<String> varNames = new ArrayList<>();
-                            while (true){
-                                System.out.printf(" %d. değişken tipini giriniz. Cikmak icin bitti yaziniz: ",varCount);
+                            while (true) {
+                                System.out.printf(" %d. değişken tipini giriniz. Cikmak icin bitti yaziniz: ",
+                                        varCount);
                                 String varType = input.nextLine();
 
-                                if(varType.equals("bitti")){
-                                    System.out.println("Cıkıs yapıldı");
+                                if (programFunctions.ControlEndLoop(varType)) {
                                     break;
                                 }
-                                
 
-                                String protoVarType="";
-                                switch (programmingLanguage){
+                                String protoVarType = "";
+                                switch (programmingLanguage) {
                                     case "C#":
-                                        protoVarType=getVarType(csharp,varType);
+                                        protoVarType = programFunctions.getVarType(csharp, varType);
                                         break;
                                     case "C++":
-                                        protoVarType=getVarType(cpp,varType);
+                                        protoVarType = programFunctions.getVarType(cpp, varType);
                                         break;
                                     case "Java":
-                                        protoVarType=getVarType(java,varType);
+                                        protoVarType = programFunctions.getVarType(java, varType);
                                         break;
-                                    case  "Python":
-                                        protoVarType=getVarType(python,varType);
+                                    case "Python":
+                                        protoVarType = programFunctions.getVarType(python, varType);
                                         break;
                                     default:
                                         System.out.println("SwitchCase defalt. Cıkıs yaptı. Noluyo lan");
                                         break;
                                 }
 
-                                if (Objects.equals(protoVarType, "")){
+                                if (Objects.equals(protoVarType, "")) {
                                     System.out.println("Hatalı bir değişken türü girdiniz. Lütfen tekrar deneyin.");
                                     continue;
                                 }
-
-
-                                while (true) {
-                                    System.out.printf("%d. değişken adını giriniz: ",varCount);
-                                    String varName = input.nextLine();
-                                    if (!messageControl(varName)) {
-                                        continue;
-                                    }
-                                    if (varNames.contains(varName)){
-                                        System.out.println("Daha önce kullanılmış bir değişken adı girdiniz. Lütfen tekrar deneyin.");
-                                        continue;
-                                    }
-                                    lines.add(protoVarType+" "+varName+" = "+varCount+";");
-                                    varNames.add(varName);
-                                    break;
-                                }
-                                varCount+=1;
-                                
-
+                                programFunctions.AddVarToMessageLoop(input, varCount, varNames, lines, protoVarType);
+                                varCount += 1;
 
                             }
                             lines.add("}\n\n");
                             Files.write(path, lines);
                         }
-                        lines.add("service "+protoFileName+" {");
-
-                        while (true){
+                        lines.add(programFunctions.AddService(protoFileName));
+                        List<String> rpcNames = new ArrayList<>();
+                        while (!messageNames.isEmpty()) {
                             System.out.print("RPC adını giriniz. Cikmak icin bitti yaziniz: ");
                             String rpcName = input.nextLine();
-                            if(rpcName.equals("bitti")){
-                                System.out.println("Cıkıs yapıldı");
+
+                            if (programFunctions.ControlEndLoop(rpcName)) {
                                 break;
                             }
-                            if(!messageControl(rpcName)){
+
+                            if (!ProgramFunctions.ControlFunctions.messageControl(rpcName) ||
+                                    (programFunctions.ControlListContain(rpcNames,
+                                            "Daha önce kullanılmış bir RPC adı girdiniz. Lütfen tekrar deneyin.",
+                                            rpcName)) ) {
                                 continue;
                             }
                             System.out.println("Mesajlar:");
 
-                            for (String mesageN: messageList){
-                                System.out.print(mesageN+" ");
+                            for (String mesageN : messageNames) {
+                                System.out.print(mesageN + " ");
                                 System.out.println();
                             }
 
                             System.out.println("Önce client, sonra server mesaj ismini girin.");
-                            while (true) {
-                                System.out.print("Client mesajı: ");
-                                String clientMessage = input.nextLine();
-                                if (!messageList.contains(clientMessage)) {
-                                    System.out.println("Girdiginiz mesaj bulunamadi. Lutfen tekrar deneyin");
-                                    continue;
-                                }
-
-                                while (true) {
-                                     System.out.print("Server mesajı: ");
-                                     String serverMessage = input.nextLine();
-                                     if (!messageList.contains(serverMessage)) {
-                                    System.out.println("Girdiginiz mesaj bulunamadi. Lutfen tekrar deneyin");
-                                    continue;
-                                }
-                                     lines.add("     rpc "+rpcName+"("+clientMessage+") returns ("+serverMessage+") {}");
-
-                                     break;
-                                }
-                                break;
-                            }
+                            programFunctions.AddRpcLoop(input, messageNames, lines, rpcName);
+                            rpcNames.add(rpcName);
 
 
                         }
                         lines.add("}\n");
                         Files.write(path, lines);
-                        //sayı almak yerıne sınırsız gırıs hakkı verelım kac kelime girerse onu sayacta tutalım
-
-
+                        // sayı almak yerıne sınırsız gırıs hakkı verelım kac kelime girerse onu sayacta
+                        // tutalım
 
                     } catch (RuntimeException e) {
                         throw new RuntimeException(e);
                     }
                 }
             } catch (Exception e) {
-                e.printStackTrace(); 
+                e.printStackTrace();
             }
 
+        }
 
-   }
+    }
 
-}
 
 }
---
+++ JavaP/ProgramFunctions.java
@@ -0,0 +1,217 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ProgramFunctions {
+    private static final String[] RESERVED_KEYWORDS = { "int", "double", "float", "char", "boolean", "String" };
+
+    public String getVarType(ScalarValuesCSharp base, String input) {
+        try {
+            for (String key : base.scalarValuesMap.keySet()) {
+                String value = base.scalarValuesMap.get(key);
+                if (key.equals(input)) {
+                    System.out.println(value);
+                    return value;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+
+    public String getVarType(ScalarValuesCPP base, String input) {
+        try {
+            for (String key : base.scalarValuesMap.keySet()) {
+                String value = base.scalarValuesMap.get(key);
+                if (key.equals(input)) {
+                    System.out.println(value);
+                    return value;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+
+    public String getVarType(ScalarValuesJAVA base, String input) {
+        try {
+            for (String key : base.scalarValuesMap.keySet()) {
+                String value = base.scalarValuesMap.get(key);
+                if (key.equals(input)) {
+                    System.out.println(value);
+                    return value;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+
+    public String getVarType(ScalarValuesPython base, String input) {
+        try {
+            for (String key : base.scalarValuesMap.keySet()) {
+                String value = base.scalarValuesMap.get(key);
+                if (key.equals(input)) {
+                    System.out.println(value);
+                    return value;
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return "";
+    }
+
+    public class ControlFunctions {
+        static boolean messageControl(String inputMessage) {
+            if (inputMessage.isEmpty()
+                    || !Character.isLetter(inputMessage.charAt(0))
+                    || inputMessage.contains(" ")
+                    || Arrays.asList(RESERVED_KEYWORDS).contains(inputMessage)
+                    || inputMessage.matches(".*[üşöçİğÜŞÖÇıĞ].*"))
+
+            {
+                System.out.println(
+                        "Hata: Girişte başında sayı , özel karakter , Türkçe harf , aralarda boşluk ve özel karakter bulunmamalıdır.");
+                return false;
+            }
+            ;
+            return true;
+        }
+    }
+
+
+    public String makeFirstCharBig(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        String kucukHarfString = str.toLowerCase();
+        return Character.toUpperCase(kucukHarfString.charAt(0)) + kucukHarfString.substring(1);
+    }
+
+    public boolean ControlProgramingLanguage(String programmingLanguage) {
+        return !programmingLanguage.equals("C#") && !programmingLanguage.equals("C++")
+                && !programmingLanguage.equals("Java") && !programmingLanguage.equals("Python");
+    }
+
+    public String ChooseProgramingLanguage(Scanner input) {
+        System.out.print("Please write your programming language (C#, C++, Java, Python): ");
+        String programmingLanguage = input.nextLine();
+        programmingLanguage = makeFirstCharBig(programmingLanguage);
+        while (ControlProgramingLanguage(programmingLanguage)) {
+            System.out.println("Gecersiz dil girdiniz. Lutfen tekrar giriniz.");
+            System.out.print("Please write your programming language (C#, C++, Java, Python): ");
+            programmingLanguage = input.nextLine();
+        }
+        return programmingLanguage;
+    }
+
+    public String SetProtoFileName(Scanner input) {
+        while (true) {
+            String protoFileName;
+            System.out.print("Please write your proto file name: ");
+            protoFileName = input.nextLine();
+            if (!ControlFunctions.messageControl(protoFileName)) {
+                continue;
+            }
+            return protoFileName;
+        }
+    }
+
+    public boolean ControlEndLoop(String bitti) {
+        if (bitti.equals("bitti")) {
+            System.out.println("Cıkıs yapıldı");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean ControlListContain(List<String> controlList, String WarningMessage, String controlStr) {
+        if (controlList.contains(controlStr)) {
+            System.out.println(WarningMessage);
+            return true;
+        }
+        return false;
+    }
+    public boolean ControlListNotContain(List<String> controlList, String WarningMessage, String controlStr) {
+        if (controlList.contains(controlStr)) {
+            return false;
+        }
+        System.out.println(WarningMessage);
+        return true;
+    }
+
+    public String AddRpc(String rpcName, String clientMessage, String serverMessage) {
+        return "     rpc " + rpcName + "(" + clientMessage + ") returns ("
+                + serverMessage + ") {}";
+    }
+    public String AddService(String protoFileName) {
+        return "service " + protoFileName + " {";
+    }
+
+    public String AddVarToMessage(String protoVarType, String varName, Integer varCount) {
+        return protoVarType + " " + varName + " = " + varCount + ";";
+    }
+
+    public void AddBaseStringToProto(List<String> lines, String protoFileName) {
+        lines.add("syntax = \"proto3\";");
+        lines.add("");
+        lines.add("package " + protoFileName + ";");
+        lines.add("\n");
+    }
+
+    public String OpenRpcMessage(String girilenMessage) {
+        return "message " + girilenMessage + "{ ";
+    }
+
+    public void AddVarToMessageLoop(Scanner input, Integer varCount, List<String> varNames, List<String> lines,
+            String protoVarType) {
+        while (true) {
+            System.out.printf("%d. değişken adını giriniz: ", varCount);
+            String varName = input.nextLine();
+            if (!ControlFunctions.messageControl(varName)) {
+                continue;
+            }
+
+            if (ControlListContain(varNames,
+                    "Daha önce kullanılmış bir değişken adı girdiniz. Lütfen tekrar deneyin.",
+                    varName)) {
+                continue;
+            }
+
+            lines.add(AddVarToMessage(protoVarType, varName, varCount));
+            varNames.add(varName);
+            break;
+        }
+    }
+
+    public void AddRpcLoop(Scanner input, List<String> messageList, List<String> lines,
+            String rpcName) {
+        while (true) {
+            System.out.print("Client mesajı: ");
+            String clientMessage = input.nextLine();
+
+            if (ControlListNotContain(messageList,
+                    "Girdiginiz mesaj bulunamadi. Lutfen tekrar deneyin", clientMessage)) {
+                continue;
+            }
+
+            while (true) {
+                System.out.print("Server mesajı: ");
+                String serverMessage = input.nextLine();
+                if (ControlListNotContain(messageList,
+                        "Girdiginiz mesaj bulunamadi. Lutfen tekrar deneyin", serverMessage)) {
+                    continue;
+                }
+                lines.add(AddRpc(rpcName, clientMessage, serverMessage));
+
+                break;
+            }
+            break;
+        }
+    }
+}
